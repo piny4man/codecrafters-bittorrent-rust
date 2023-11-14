c1908a2981c96016bdfa8909e5995258c0b8052d
piny4man <development@pinya.dev>
2023-11-14 19:57:39 +0100

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
         return serde_json::Value::String(string.to_string());
     } else if start_with == 'i' {
-        let decoded: i32 = serde_bencode::from_str(&encoded_value).unwrap();
+        let decoded: i64 = serde_bencode::from_str(&encoded_value).unwrap();
         return serde_json::Value::Number(decoded.into());
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
